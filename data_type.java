51af0b7dd3b839e317ef267e344b70bca3471a76
Aatrox77 <112231881+Aatrox77@users.noreply.github.com>
2022-09-10 00:15:17 +0900

    Add files via upload

---
+++ data_type.java
@@ -0,0 +1,10 @@
+public class data_type {
+	public static void main(String[]args) {
+		System.out.println(6); //6
+		System.out.println("6"); // String 6
+		System.out.println(6+6); //12
+		System.out.println("6+6"); // String 66
+		System.out.println(6*6); //36
+		System.out.println("1111".length()); //4
+	}
+}
