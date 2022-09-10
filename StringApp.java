19c639bbe2a95f100a1ed11634b5a6b7f9656b2b
Aatrox77 <112231881+Aatrox77@users.noreply.github.com>
2022-09-10 14:38:52 +0900

    Add files via upload

---
+++ StringApp.java
@@ -0,0 +1,16 @@
+package data_and_operation;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+System.out.println("Hello World"); //String
+System.out.println('H'); //Character
+
+System.out.println("Hello "
+		+ "World");
+System.out.println("Hello \nWorld");
+System.out.println("Hello \"World\"");
+
+	}
+
+}
