71a1ec0db760f3c0579d76e40a761f2aa8d89865
Aatrox77 <112231881+Aatrox77@users.noreply.github.com>
2022-09-10 19:46:48 +0900

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,19 @@
+package data_and_operation;
+
+public class Casting {
+
+	public static void main(String[] args) {
+
+		double a= 1.1;
+		double b=1;
+		System.out.println(b);
+		
+		double c=1.1;
+		int d = (int) 1.1; 
+		System.out.println(d);
+		// 1 to String
+		String e = Integer.toString(1);
+		System.out.println(e.getClass());
+	}
+
+}
